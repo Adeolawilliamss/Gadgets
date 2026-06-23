566168b85e0cd0d5d7930108ce88752c3c7160f8
Adeolawilliamss <babagenerah@gmail.com>
2026-06-23 14:45:40 +0100

    frontend login changes

--- frontend/src/page/login/Login.js
+++ frontend/src/page/login/Login.js
@@ -7,7 +7,6 @@ import Slider from 'react-slick';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './Login.css';
 
 function Login() {
   const { showAlert } = useAlert();
@@ -78,104 +77,186 @@ function Login() {
   };
 
   return (
-    <div className="container mx-auto px-4">
-      <div className="flex flex-col bg-gray-100 md:flex-row items-center gap-10 md:gap-20 overflow-hidden pb-8">
-        <div className="slider__container w-full md:w-1/2 h-full text-center">
-          <Slider {...settings}>
-            {images.map((item) => (
-              <div key={item.id}>
-                <figure className="animated">
-                  <img
-                    src={item.img}
-                    alt={item.text}
-                    className="w-9/10 h-full"
-                  />
-                </figure>
-                <h3 className="text-center font-extrabold mb-5">{item.text}</h3>
-                <p>{item.sub}</p>
-              </div>
-            ))}
-          </Slider>
-        </div>
-
-        <div className="w-full md:w-1/2 mt-8 md:mt-10">
-          <h1 className="text-2xl md:text-4xl">Hi, Welcome Back</h1>
-          <form className="login__form" onSubmit={handleLogin}>
-            <div className="main__input">
-              <input
-                type="email"
-                value={email}
-                onChange={handleEmailChange}
-                placeholder="Enter your email"
-                className="block w-full h-12 mt-8 p-2.5 pr-10"
-              />
-              <p className="text-slate-900 text-sm mt-2">
-                For Testing: Adeolaoladeinde6@gmail.com
-              </p>
-
-              <div className="form__input">
-                <input
-                  type={showPassword ? 'text' : 'password'}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="Enter your password"
-                  minLength={8}
-                  className="input__field"
-                />
-                <span
-                  className="absolute right-2.5 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                  onClick={toggleShowPassword}
-                >
-                  {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </span>
-              </div>
-              <p className="text-slate-900 text-sm mt-2">test1234</p>
-            </div>
-            <div className="flex items-center justify-between mt-4">
-              <label htmlFor="checkbox">
-                <input type="checkbox" id="checkbox" className="mr-2" />
-                Remember Me?
-              </label>
-              <span className="cursor-pointer text-blue-500">
-                Forgot Password
-              </span>
-            </div>
-            <button
-              className="rounded-md mb-8 w-full md:w-80 h-12 mt-4 bg-[#2B2D42] text-white text-lg md:text-xl"
-              type="submit"
-            >
-              Sign In
-            </button>
-            <div className="relative w-full h-12 mt-4">
-              <button
-                className="rounded-md w-full h-full flex items-center justify-center"
-                type="button"
-              >
+  <div className="bg-gray-100 min-h-screen mx-auto px-4">
+    <div className="flex flex-col md:flex-row items-center gap-10 md:gap-10 overflow-hidden pb-8">
+      <div className="w-full md:w-1/2 h-full text-center">
+        <Slider {...settings}>
+          {images.map((item) => (
+            <div key={item.id}>
+              <figure>
                 <img
-                  src="./photos/google.png"
-                  alt="google logo"
-                  className="google-logo"
+                  src={item.img}
+                  alt={item.text}
+                  className="w-[90%] h-full mx-auto"
                 />
-              </button>
-              <span className="flex absolute top-0 left-20 lg:left-52 items-center h-full w-full text-slate-100 text-xl text-bold">
-                Sign In with Google
-              </span>
-            </div>
-          </form>
+              </figure>
 
-          <h5 className="mt-9 text-center text-lg md:text-xl">
-            Don't have an account?{' '}
-            <span
-              className="text-blue-500 cursor-pointer"
-              onClick={() => navigate('/signup')}
+              <h3 className="text-center font-extrabold mb-5 text-xl">
+                {item.text}
+              </h3>
+
+              <p className="text-gray-600">{item.sub}</p>
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+      <div className="w-full md:w-1/2 mt-8 md:mt-10 md:mr-10">
+        <h1 className="text-2xl md:text-4xl font-bold">
+          Hi, Welcome Back
+        </h1>
+
+        <form
+          className="flex flex-col"
+          onSubmit={handleLogin}
+        >
+          <div>
+            <input
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              placeholder="Enter your email"
+              className="
+                block
+                w-full
+                h-12
+                mt-8
+                px-4
+                bg-white
+                rounded-md
+                border
+                border-gray-300
+                outline-none
+                focus:ring-2
+                focus:ring-[#2B2D42]
+                focus:border-transparent
+              "
+            />
+
+            <p className="text-slate-900 text-sm mt-2">
+              For Testing: Adeolaoladeinde6@gmail.com
+            </p>
+
+            <div className="relative">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter your password"
+                minLength={8}
+                className="
+                  block
+                  w-full
+                  h-12
+                  mt-8
+                  px-4
+                  pr-12
+                  bg-white
+                  rounded-md
+                  border
+                  border-gray-300
+                  outline-none
+                  focus:ring-2
+                  focus:ring-[#2B2D42]
+                  focus:border-transparent
+                "
+              />
+
+              <button
+                type="button"
+                onClick={toggleShowPassword}
+                className="
+                  absolute
+                  right-4
+                  top-1/2
+                  -translate-y-1/2
+                  text-gray-500
+                  hover:text-gray-700
+                "
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+
+            <p className="text-slate-900 text-sm mt-2">
+              test1234
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between mt-4">
+            <label
+              htmlFor="checkbox"
+              className="flex items-center gap-2 cursor-pointer"
             >
-              Sign up for free
+              <input
+                type="checkbox"
+                id="checkbox"
+              />
+              Remember Me?
+            </label>
+
+            <span className="cursor-pointer text-blue-500 hover:underline">
+              Forgot Password
             </span>
-          </h5>
-        </div>
+          </div>
+
+          <button
+            className="
+              rounded-md
+              mb-3
+              w-full
+              md:w-100
+              h-12
+              mt-4
+              bg-[#2B2D42]
+              text-white
+              text-lg
+              md:text-xl
+              font-medium
+              hover:opacity-90
+              transition
+            "
+            type="submit"
+          >
+            Sign In
+          </button>
+
+          <button
+            type="button"
+            className="
+              w-full
+              h-12
+              flex
+              items-center
+              justify-center
+              gap-3
+              rounded-md
+              border
+              border-gray-300
+              bg-white
+              text-[#2B2D42]
+              font-semibold
+              hover:bg-gray-50
+              transition
+            "
+          >
+            <span>Sign In with Google</span>
+          </button>
+        </form>
+
+        <h5 className="mt-9 text-center text-lg md:text-xl">
+          Don't have an account?{' '}
+          <span
+            className="text-blue-500 cursor-pointer hover:underline"
+            onClick={() => navigate('/signup')}
+          >
+            Sign up for free
+          </span>
+        </h5>
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default Login;
